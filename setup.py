858f40c096788efb1b825f4e379ed6be16eb272f
Willem Jan Willemse <w.j.willemse@xs4all.nl>
2019-11-06 14:22:07 +0100

    setup.py updated

--- setup.py
+++ setup.py
@@ -20,14 +20,14 @@ test_requirements = [ ]
 setup(
     author="De Nederlandsche Bank",
     author_email='ECDB_berichten@dnb.nl',
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    python_requires='>=3.0, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Financial and Insurance Industry',
         'Intended Audience :: Developers',
+        'Intended Audience :: Education',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
